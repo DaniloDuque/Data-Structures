f8ac5ed398a05bc4569772ff4f4eb577125f6810
Danilo <105374584+DaniloDuque@users.noreply.github.com>
2023-11-23 13:57:06 -0600

    Create Queue.h

---
+++ StackNQueue/Queue.h
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+
+
+struct node{
+
+    int data;
+    node *nxt;
+    node(int x): data(x), nxt(nullptr){}
+    ~node() {}
+
+};
+
+
+
+class queue{
+
+
+    private:
+
+        node * Front;
+        node * Back;
+
+
+    public:
+
+        queue(): Front(nullptr), Back(nullptr){}
+        void push(int);
+        void pop();
+        int front();
+        bool empty(){return !Front;}
+
+};
+
+
+
+void queue::push(int x){
+
+    node * newN = new node(x);
+    if(!Front){
+
+        Front = newN;
+        Back = newN;
+        return;
+
+    }Back->nxt = newN;
+    Back = newN;
+    return;
+
+}
+
+
+void queue::pop() {
+
+    int x = Front->data;
+    Front = Front->nxt;
+
+}
+
+
+int queue::front(){
+
+    return Front->data;
+    
+}
