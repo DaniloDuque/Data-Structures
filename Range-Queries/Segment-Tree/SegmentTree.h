cdf84387195b8e26ded9ffeb76c45cedfba7ccfd
Danilo <105374584+DaniloDuque@users.noreply.github.com>
2024-10-18 13:31:34 -0600

    Update SegmentTree.h

--- Range-Queries/Segment-Tree/SegmentTree.h
+++ Range-Queries/Segment-Tree/SegmentTree.h
@@ -1,36 +1,42 @@
+template<typename T>
 class SegmentTree{
 
 private:
-    vector<int> st; int n, NEUT;
-    function<int(int, int)>fun;
+    vector<T> st; 
+    int n; 
+    T NEUT;
+    function<T(T, T)> fun;
 
-    void init(int k, int s, int e, int *a){
-        if(s+1 == e){st[k]==a[s]; return;}
-        int m = (s+e)>>1;
-        init(k<<1, s, m, a); init((k<<1)+1, m, e, a);
-        st[k]=fun(st[k<<1], st[(k<<1)+1];
+    void init(int k, int s, int e, T *a){
+        if(s+1 == e) { st[k] = a[s]; return; }
+        int m = (s+e) >> 1;
+        init(k<<1, s, m, a); 
+        init((k<<1)+1, m, e, a);
+        st[k] = fun(st[k<<1], st[(k<<1)+1]);
     }
 
-    void upd(int k, int s, int e, int p, int v){
-        if(s+1==e){st[k]==v; return;}
-        int m=(s+e)>>1;
-        (p<m)? upd(k<<1, s, m, p, v) : upd((k<<1)+1, m, e, p, v);
-        st[k]=fun(st[k<<1], st[(k<<1)+1]);
+    void upd(int k, int s, int e, int p, T v){
+        if(s+1 == e) { st[k] = v; return; }
+        int m = (s+e) >> 1;
+        (p < m) ? upd(k<<1, s, m, p, v) : upd((k<<1)+1, m, e, p, v);
+        st[k] = fun(st[k<<1], st[(k<<1)+1]);
     }
 
-    int query(int k, int s, int e, int a, int b){
-	    if(s>=b||e<=a)return NEUT;
-	    if(s>=a&&e<=b)return st[k];
-	    int m=(s+e)>>1;
-	    return fun(query(k<<1,s,m,a,b),query((k<<1)+1,m,e,a,b));
+    T query(int k, int s, int e, int a, int b){
+        if(s >= b || e <= a) return NEUT;
+        if(s >= a && e <= b) return st[k];
+        int m = (s+e) >> 1;
+        return fun(query(k<<1, s, m, a, b), query((k<<1)+1, m, e, a, b));
     }
- 
+
 public:
-    SegmentTree(int sz, int neut, function<int(int, int)> oper){
-        n = sz; NEUT = neut; st(4*n+5, NEUT); fun = oper; 
+    SegmentTree(int sz, T neut, function<T(T, T)> oper) {
+        n = sz; NEUT = neut; 
+        st.assign(4 * n + 5, NEUT); 
+        fun = oper; 
     }
-    void init(int *a){init(1, 0, n, a);}
-    void upd(int i, int v){upd(1, 0, n, i, v);}
-    int query(int i, int j){return query(1, 0, n, i, j);}
-
+    
+    void init(T *a) { init(1, 0, n, a); }
+    void upd(int i, T v) { upd(1, 0, n, i, v); }
+    T query(int i, int j) { return query(1, 0, n, i, j); }
 };
