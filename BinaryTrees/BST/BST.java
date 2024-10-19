d36b9600bc339c59958c0d5a9ac8d1338945a8c4
Danilo <105374584+DaniloDuque@users.noreply.github.com>
2024-10-18 18:15:17 -0600

    Update BST.java

--- BinaryTrees/BST/BST.java
+++ BinaryTrees/BST/BST.java
@@ -1,9 +1,9 @@
-public class BST{
+public class BST<T extends Comparable<T>> {
 
-    private class node{
-        int data;
+    private class node {
+        T data;
         node left, right;
-        node(int x){
+        node(T x) {
             data = x;
             left = right = null;
         }
@@ -11,39 +11,44 @@ public class BST{
 
     private node root;
 
-    private node insert(node root, node newN){
-        if(root == null) return newN;
-        if(root.data < newN.data) root.right = insert(root.right, newN);
+    private node insert(node root, node newN) {
+        if (root == null) return newN;
+        if (root.data.compareTo(newN.data) < 0) root.right = insert(root.right, newN);
         else root.left = insert(root.left, newN);
         return root;
     }
 
-    private int getMinValue(node root){
-        if(root.left == null) return root.data;   
+    private T getMinValue(node root) {
+        if (root.left == null) return root.data;
         return getMinValue(root.left);
     }
 
-    private node remove(node root, int x){
-        if(root == null) return null;    //if the node we are searching doesnt exist, do not remove anything
-        if(root.data == x){
-            if(root.right != null && root.left != null){  //this is the case root has 2 child
-                int min = getMinValue(root.right);
+    private node remove(node root, T x) {
+        if (root == null) return null;
+        if (root.data.compareTo(x) == 0) {
+            if (root.right != null && root.left != null) {
+                T min = getMinValue(root.right);
                 root.data = min;
                 root.right = remove(root.right, min);
-            }else return (root.right != null)? root.right: root.left;   //returns the child of root that exists
-        }else if(root.data < x) root.right = remove(root.right, x);
-        else root.left = remove(root.left, x);
+            } else {
+                return (root.right != null) ? root.right : root.left;
+            }
+        } else if (root.data.compareTo(x) < 0) {
+            root.right = remove(root.right, x);
+        } else {
+            root.left = remove(root.left, x);
+        }
         return root;
     }
 
-    private boolean search(node root, int x){
-        if(root == null) return false;
-        if(root.data == x) return true;
-        if(root.data < x) return search(root.right, x);
+    private boolean search(node root, T x) {
+        if (root == null) return false;
+        if (root.data.compareTo(x) == 0) return true;
+        if (root.data.compareTo(x) < 0) return search(root.right, x);
         return search(root.left, x);
     }
 
-    private void display(node root, String prefix, boolean isLeft){
+    private void display(node root, String prefix, boolean isLeft) {
         if (root == null) return;
         System.out.print(prefix);
         System.out.print((isLeft ? "|---" : "---"));
@@ -52,16 +57,23 @@ public class BST{
         display(root.left, prefix + (isLeft ? "|    " : "     "), false);
     }
 
-    public BST(){
+    public BST() {
         root = null;
     }
 
-    public void insert(int x){root = insert(root, new node(x));}
+    public void insert(T x) {
+        root = insert(root, new node(x));
+    }
 
-    public void remove(int x){root = remove(root, x);}
+    public void remove(T x) {
+        root = remove(root, x);
+    }
 
-    public boolean search(int x){ return search(root, x);}
+    public boolean search(T x) {
+        return search(root, x);
+    }
 
-    public void display(){display(root, "", true);}
-
+    public void display() {
+        display(root, "", true);
+    }
 }
